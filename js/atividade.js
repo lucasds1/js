498c2ef1ad5b021ecccd9e719fdd60ac6ffe347b
lucasds1 <lucashenri34567@gmail.com>
2022-07-26 08:46:30 -0300

    lucas

---
+++ js/atividade.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+
+
+    $('#bto-1-5').click(function(){
+
+
+
+
+        $('p').removeClass('font-2-5')
+
+        $('p').removeClass('font-2-0')
+
+        $('p').addClass('font-1-5');
+
+
+
+    });
+
+
+
+    $('#bto-2-0').click(function(){
+
+        $('p').removeClass('font-2-5')
+
+        $('p').removeClass('font-1-5')
+
+        $('p').addClass('font-2-0');
+
+    });
+
+
+
+    $('#bto-2-5').click(function(){
+
+        $('p').removeClass('font-1-5')
+
+        $('p').removeClass('font-2-0')
+
+        $('p').addClass('font-2-5');
+
+    });
+
+
+
+});
